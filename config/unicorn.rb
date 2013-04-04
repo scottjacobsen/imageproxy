110e14f3eb701e8f7f288f4423f4b7512685146c
Anders Bengtsson <anders.bengtsson@valtech.se>
2013-04-04 10:14:05 +0200

    Lower Unicorn timeout to 15s
    No point in hanging for 30s when something is slow further down.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,3 @@
 preload_app true
 worker_processes 3 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
+timeout 15         # restarts workers that hang for 30 seconds
