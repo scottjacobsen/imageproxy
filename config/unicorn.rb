3910f1e848a4443c2f7455f5b10e648a72acc851
Anders Bengtsson <anders.bengtsson@valtech.se>
2013-04-09 10:53:10 +0200

    Fuck everything, let's have 5 workers!

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,3 @@
 preload_app true
-worker_processes 4 # amount of unicorn workers to spin up
+worker_processes 5 # amount of unicorn workers to spin up
 timeout 15         # restarts workers that hang for 30 seconds
